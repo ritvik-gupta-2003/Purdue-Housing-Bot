c912598c0df3051f7b4a43857cd91b82829fa0ae
Caleb Buening <caleb.j.buening@gmail.com>
2023-12-05 09:14:00 -0500

    Rearranged a lot of the script.

--- main.py
+++ main.py
@@ -1,99 +1,86 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
 from discordwebhook import Discord
-
-discord = Discord(url = "https://discordapp.com/api/webhooks/1042691320956858411/XMy93U4dSTN34NPq8qk8k4zGk0Uv391Swfdv-ThBTFs5UD3tSF2KuoARpLXmEBW06Wc4")
-
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
+try:
+    # Load in the important variables
+    discordURL = input("Enter Discord Webhook URL: ")
 
-chrome_options.add_experimental_option("detach", True)
+    # The tools we will use
+    discord = Discord(url = discordURL)
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
+    # Announce that we have begun
+    discord.post(content="Webhook Running")
+    print("Webhook Running")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    # State variables to prevent multiple notifications
+    hilly = False
+    frieda = False
+    winifred = False
+    cary = False
+    hawkins = False
+    earehart = False
 
-driver.get("https://myhousing.purdue.edu/StarRezPortalX/8D7E8611/1/1/Home-Home")
+    input(
+    """
+    Please log in to the housing portal and navigate
+    to the list of available dorms. Then,
+    press enter to begin monitoring...
+    """)
 
-
-loginButton = driver.find_element("link text", "Log In")
-loginButton.click()
-
-usernameInput = driver.find_element(By.ID, "username")
-passwordInput = driver.find_element(By.ID, "password")
-
-time.sleep(0.5)
-
-usernameInput.send_keys("gupta897")
-passwordInput.send_keys("2022,push")
-
-time.sleep(1)
-
-submitButton = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/form/fieldset/div[3]/div[2]/input[4]")
-submitButton.click()
-
-time.sleep(12)
-
-
-contractButton = driver.find_element("link text", "Contract Renewal (Renew with University Residences)")
-contractButton.click()
-
-time.sleep(3)
-
-continueButton = driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div/article/div/div/div/section/div[1]/section/form/div/div[2]/div[2]/button")
-continueButton.click()
-
-time.sleep(3)
-
-proceedButton = driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div/article/div/div/div/section/div[2]/button")
-proceedButton.click()
-
-time.sleep(3)
-
-roomSelection = driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div/article/div/div/section/div/div/div[2]/div/a[7]/div[1]")
-roomSelection.click()
-
-time.sleep(3)
-
-changeRoomLink = driver.find_element(By.XPATH, "/html/body/div[1]/section[1]/div/article/div/div/div/section/div[1]/div[7]/div[3]/div/div/span/ul[1]/li/strong[1]/a")
-changeRoomLink.click()
-
-time.sleep(3)
-
-
-condition = True
-
-hilly = False
-frieda = False
-winifred = False
-
-
-while (condition):
-
-    if "Hillenbrand" in driver.page_source:
-
-        if hilly == False:
-            discord.post(content="Hillenbrand Available!")
+    while (True):
+        # Alerts for appearances and disappearances
+        if "Hillenbrand" in driver.page_source and not hilly:
+            discord.post(content="Hilly Available!")
             hilly == True
-
-    else:
-
-        if hilly == True:
-            discord.post(content="Hillenbrand Gone!")
+        if not ("Hillenbrand" in driver.page_source) and hilly:
+            discord.post(content="Hilly Gone!")
             hilly == False
 
-    if "Winifred" in driver.page_source:
-        discord.post(content="Winifred Parker Available!")
+        if "Winifred" in driver.page_source and not winifred:
+            discord.post(content="Winifred Available!")
+            winifred = True
+        if not ("Winifred" in driver.page_source) and winifred:
+            discord.post(content="Winifred Gone!")
+            winifred = False
 
-    if "Frieda" in driver.page_source:
-        discord.post(content="Frieda Parker Available!")
+        if "Frieda" in driver.page_source and not frieda:
+            discord.post(content="Frieda Available!")
+            frieda = True
+        if not ("Frieda" in driver.page_source) and frieda:
+            discord.post(content="Frieda Gone!")
+            frieda = False
 
+        if "Cary" in driver.page_source and not cary:
+            discord.post(content="Cary Available!")
+            cary = True
+        if not ("Cary" in driver.page_source) and cary:
+            discord.post(content="Cary Gone!")
+            cary = False
+        if "Hawkins" in driver.page_source and not hawkins:
+            discord.post(content="Hawkins Available!")
+            hawkins == True
+        if not ("Hawkins" in driver.page_source) and hawkins:
+            discord.post(content="Hawkins Gone!")
+            hawkins == False
+        if "Earhart" in driver.page_source and not earehart:
+            discord.post(content="Earhart Available!")
+            earehart == True
+        if not ("Earhart" in driver.page_source) and earehart:
+            discord.post(content="Earhart Gone!")
+            earehart == False
 
-    driver.refresh()
+        driver.refresh()
+        time.sleep(5)
 
-    time.sleep(3)
+except Exception as e:
+    discord.post(content="Error: " + str(e))
+    print("Error: " + str(e))
+    driver.quit()
